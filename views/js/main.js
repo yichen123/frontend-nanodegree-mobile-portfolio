10720d237f43556667ac80d6f6d78521103bf96c
yichen123 <chenyi330@hotmail.com>
2015-07-06 22:38:30 +0800

    reduced number of sliding pizzas

--- views/js/main.js
+++ views/js/main.js
@@ -544,9 +544,10 @@ window.addEventListener('scroll', updatePositions);
 
 // Generates the sliding pizzas when the page loads.
 document.addEventListener('DOMContentLoaded', function() {
-
-    var cols = 8;
+    var screenWidth = document.body.clientWidth;
+    var screenHeight = window.screen.availHeight;
     var s = 256;
+    var cols = Math.ceil(screenWidth / s) + 1;
     for (var i = 0; i < 200; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
@@ -554,8 +555,12 @@ document.addEventListener('DOMContentLoaded', function() {
         elem.style.height = "100px";
         elem.style.width = "73.333px";
         elem.basicLeft = (i % cols) * s;
-        elem.style.top = (Math.floor(i / cols) * s) + 'px';
+        var elemTop = (Math.floor(i / cols) * s);
+        elem.style.top = elemTop + 'px';
         document.querySelector("#movingPizzas1").appendChild(elem);
+        if (elemTop > screenHeight) {
+            break;
+        }
     }
     updatePositions();
 });
